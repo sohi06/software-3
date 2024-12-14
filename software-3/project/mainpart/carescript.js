3250fd1720515429fe02a5178613756446e20ede
Sohi Bilkis BInte Yer <sbby740@gmail.com>
2024-12-14 09:17:51 +0600

    Add files via upload
    all merged file

---
+++ software-3/project/mainpart/carescript.js
@@ -0,0 +1,6 @@
+// Placeholder JavaScript for handling "Add to Cart" button
+document.querySelectorAll('.product button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Item added to cart!');
+    });
+});//sithi
