2bbe444500e37ca8d510f4b1d16339bfbca9e9b7
Sohi Bilkis BInte Yer <sbby740@gmail.com>
2024-12-25 17:29:05 +0600

    Delete software-3 directory

--- software-3/project/mainpart/carescript.js
+++
@@ -1,6 +0,0 @@
-// Placeholder JavaScript for handling "Add to Cart" button
-document.querySelectorAll('.product button').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Item added to cart!');
-    });
-});//sithi
